42701b63a28197628314420f3df504890ed6438f
Ben Thomas <ben.thomas1984@gmail.com>
2024-05-14 12:23:09 +0100

    Cloud init name

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "random_id" "secret" {
 }
 
 resource "harvester_cloudinit_secret" "cloud-config" {
-  name      = "cloud-config-${var.prefix}${random_id.secret.id}"
+  name      = "cloud-config-${random_id.secret.id}-${var.prefix}"
   namespace = var.namespace
 
   user_data = templatefile("cloud-init.tmpl.yml", {
